3405338950aa1a276a809efb37d39e63d6a79ef7
NguyenCatNguyen <catngn11@gmail.com>
2025-03-27 16:02:25 -0500

    Update Navbar.jsx

--- src/section/Navbar.jsx
+++ src/section/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
     <header className='fixed top-0 left-0 right-0 z-50 bg-black/90'>
         <div className='max-w-7xl mx-auto'>
             <div className='flex justify-between items-center py-5 mx-auto c-space'>
-                    <a href="/" className='text-neutral-400 font-bold texl-xl hover:text-white transition-color'>Cat Nguyen</a>
+                    <a href="https://github.com/NguyenCatNguyen" className='text-neutral-400 font-bold texl-xl hover:text-white transition-color'>Cat Nguyen</a>
                     <button onClick={toggleMenu} className='text-neutral-400 hover:text-white transition-colors focus:outline-none sm:hidden flex'>
                         <img src={isOpen ? "assets/close.svg" : "assets/menu.svg"} alt="toggle" className='size-6' />
                     </button>
