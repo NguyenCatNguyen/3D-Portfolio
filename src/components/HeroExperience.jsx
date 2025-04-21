627d378eaf6dd6b797be9011c21db005727053eb
NguyenCatNguyen <catngn11@gmail.com>
2025-04-20 22:31:38 -0500

    update

--- src/components/HeroExperience.jsx
+++ src/components/HeroExperience.jsx
@@ -1,12 +1,15 @@
 import { OrbitControls } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
+import { useMediaQuery } from 'react-responsive'
 
 const HeroExperience = () => {
-    const isTablet = false
+    const isMobile = useMediaQuery({ query: "(max-width: 768px)" }); // Check if the device is mobile
+    const isTablet = useMediaQuery({ query: "(max-width: 1024px)" }); // Check if the device is tablet
+
   return (
     <Canvas  camera={{ position: [0, 0, 15], fov: 45 }}>
         <ambientLight intensity={0.2} color="#1a1a40" />
-        <directionalLight position={[5,5,5]} intensity={1} />
+        <directionalLight position={[10,20,5]} intensity={1} />
         <OrbitControls 
             enablePan = {false} // Disable panning handling
             enableZoom = {!isTablet} // Allow zoom to happen on an PC and not a tablet
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -42,7 +42,7 @@ const Hero = () => {
                 </p>
                 <Button
                 text="See My Work"
-                className="md:w-80 md:h-16 w-60 h-12"
+                className="md:w-80 md:h-16 w-60 h-12 mt-5"
                 id="counter"
                 />
             </header>
