dfc604171681f3b8ceb9d79ea45a8f0a2e5dcb5c
NguyenCatNguyen <catngn11@gmail.com>
2025-04-23 16:15:51 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Hero from './sections/Hero.jsx'
+import ShowcaseSection from './sections/ShowcaseSection.jsx'
+import NavBar from './components/NavBar.jsx'
 
 const App = () => {
   return (
     <div>
+      <NavBar/>
       <Hero/>
+      <ShowcaseSection/>
     </div>
   )
 }
